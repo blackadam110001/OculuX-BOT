ec19e99f532f17f93edaa38b51e8084a8f953896
blackadam110001 <johnlall417@gmail.com>
2024-08-02 23:41:49 +0530

    Add / commands

---
+++ commands/commands.js
@@ -0,0 +1,43 @@
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+  {
+    name: "hint",
+    description: "Get a hint for the treasure hunt",
+    options: [
+      {
+        name: "level",
+        description: "The level you need a hint for",
+        type: 4,
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "start",
+    description: "Start Hunt",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(
+  "TOKEN",
+);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands("1268936435692732478"), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
